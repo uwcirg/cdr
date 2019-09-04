6f6a05eb7e5d5396000e1a85f2007014e3b27f74
Paul Bugni <pbugni@u.washington.edu>
2019-09-04 09:23:07 -0700

    add cli endpoint to print config value

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import alembic.config
 import click
 from flask_migrate import Migrate
+import json
 import os
 import sys
 from time import sleep
@@ -42,6 +43,26 @@ def initdb():
     """Initialize database (if necessary"""
     if not sdb.engine.dialect.has_table(sdb.engine.connect(), 'alembic_version'):
         sdb.create_all()
+
+
+@click.option(
+    '--config_key',
+    '-c',
+    help='Return a single config value, or empty string if value is None'
+)
+@app.cli.command()
+def config(config_key):
+    """List current flask configuration values in JSON"""
+
+    if config_key:
+        # Remap None values to an empty string
+        print(app.config.get(config_key, '') or '')
+        return
+    print(json.dumps(
+        # Skip un-serializable values
+        {k: v for k, v in app.config.items() if isinstance(v, str)},
+        indent=2,
+    ))
 
 
 @click.option(
